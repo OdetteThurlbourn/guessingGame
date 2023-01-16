c01f3efe513a6b339f4ddf678934b001069b3db1
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-16 16:08:50 +0000

    Add 'play-again' feature

--- run.py
+++ run.py
@@ -7,23 +7,19 @@ max_range = int(input("Enter a maximum number: "))
 answer = random.randint(min_range, max_range)
 guesses = 0
 
+#Making sure guess value is an integer
 def validate_guess(guess):
     if not guess.isdigit():
        return False
     else:
       return True
 
-
 def game(min_range, max_range):
     number = random.randint(min_range, max_range)
 
-    print(f"Welcome to the guessing game! I am thinking of a number between {min_range} and {max_range}.")
-    print("Try to guess what it is.")
-
-guesses = 0
-
 
 while True:
+    #Get users guess
     guess = input("Enter your guess: ")
     if not validate_guess(guess):
         print("Invalid input, please enter a number.")
@@ -33,7 +29,7 @@ while True:
 
     hint = input("Would you like a hint? (y/n)")
 
-    if hint.lower() == "yes":
+    if hint.lower() == "y":
         if answer % 2 == 0:
             print("Hint: the answer is even.")
         else:
@@ -42,4 +38,14 @@ while True:
     guess = int(input("Guess again: "))
     guesses += 1
 
-print("Congratulations! You guess the correct number in", guesses, "tries.")
+play_again = "y"
+while play_again =="y":
+    print("Enter the range of the number you want to guess. ")
+    min_range = int(input("Enter the minimum range:"))
+    max_range = int(Input("Enter the maximum range:"))
+    if game(min_range, max_range):
+        play_again = input("Do you want to play again? (y/n) ")
+    else:
+        print("Somthing went wrong! Please try again.")
+
+print("Congratulations! You guessed the correct number in", guesses, "tries.")
