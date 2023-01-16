b2686b0b567fdd1e865fe39baa8a202adadbbc17
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-16 17:21:23 +0000

    Correct indentation errors

--- run.py
+++ run.py
@@ -7,48 +7,44 @@ max_range = int(input("Enter a maximum number: "))
 answer = random.randint(min_range, max_range)
 guesses = 0
 
-#Making sure guess value is an integer
+# Making sure guess value is an integer
 def validate_guess(guess):
     if not guess.isdigit():
-       return False
+        return False
     else:
-      return True
+        return True
 
 def game(min_range, max_range):
     number = random.randint(min_range, max_range)
     guesses = 0
-while True:
-    #Get users guess
-    guess = input("Enter your guess: ")
-    if not validate_guess(guess):
-        print("Invalid input, please enter a number.")
-        continue
-    guess = int(guess)
-    guesses += 1
+    while True:
+        # Get users guess
+        guess = input("Enter your guess: ")
+        if not validate_guess(guess):
+            print("Invalid input, please enter a number.")
+            continue
+        guess = int(guess)
+        guesses += 1
 
-    hint = input("Would you like a hint? (y/n)")
+        hint = input("Would you like a hint? (y/n)")
 
-    if hint.lower() == "y":
-        if answer % 2 == 0:
-            print("Hint: the answer is even.")
+        if hint.lower() == "y":
+            if answer % 2 == 0:
+                print("Hint: the answer is even.")
+            else:
+                print("Hint: the answer is odd.")
+
+        if guess == answer:
+            print("Congratulations! You guessed the correct number in", guesses, "tries.")
+            return True
+        elif guess > answer:
+            print("Too high! Guess again.")
         else:
-            print("Hint: the answer is odd.")
-
-    guess = int(input("Guess again: "))
-    guesses += 1
-
-    if guess == answer:
-        print("Congratulations! You guessed the correct number in", guesses, "tries.")
-      return True
-    elif guess > answer:
-        print("Too high! Guess again.")
-    else:
-        print("Too low! Guess again.")
-      return False
-
+            print("Too low! Guess again.")
+    return False
 
 play_again == "y"
-while play_again =="y":
+while play_again == "y":
     print("Enter the range of the number you want to guess. ")
     min_range = int(input("Enter the minimum range:"))
     max_range = int(Input("Enter the maximum range:"))
@@ -56,4 +52,3 @@ while play_again =="y":
         play_again = input("Do you want to play again? (y/n) ")
     else:
         print("Somthing went wrong! Please try again.")
-
