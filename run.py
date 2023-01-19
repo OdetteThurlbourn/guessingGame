e2c6ab1bc98cf2cb4ddc4a14349593c4b07a9f37
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-19 12:51:56 +0000

    Add validation to check min and max values

--- run.py
+++ run.py
@@ -1,14 +1,11 @@
 import random
 
-print("Welcome to my guessing game!")
-
 # Making sure guess value is an integer
 def validate_guess(guess):
     if not guess.isdigit():
         return False
     else:
         return True
-
 
 def game(min_range, max_range):
     correct_guesses = 0
@@ -40,7 +37,6 @@ def game(min_range, max_range):
         else:
             print("Too low! Guess again.")
 
-
         if guesses == 3 and hint_used == False:
             hint = input("Would you like a hint? (y/n)")
             if hint.lower() == "y":
@@ -53,22 +49,52 @@ def game(min_range, max_range):
             hint = input("Would you like another hint? (y/n)")
             if hint.lower() == "y":
                 hint_used2 = True
-                print("Hint: the answer is between",min_range, "and", max_range)
+                print("Hint: the answer is between", min_range, "and", max_range)
 
     return correct_guesses, total_tries, hint_used
-       
-play_again = "y"
-correct_guesses = 0
-total_tries = 0
-hint_used = False
-hint_used2 = False
-while play_again == "y":
-    print("Enter the range of the number you want to guess. ")
-    min_range = int(input("Enter the minimum range:"))
-    max_range = int(input("Enter the maximum range:"))
-    correct_guesses
-    play_again = game(min_range, max_range)
-    play_again = input("Do you want to play again? (y/n)")
-    if play_again == 'n':
-        print("Thank you for playing. Goodbye!")
-        break
+
+# The code below was in collaboration with Mentor Spencer Barriball
+if __name__ == '__main__':
+    print("Welcome to my guessing game!")
+    answer = "y"
+    correct_guesses = 0
+    total_tries = 0
+    hint_used = False
+    hint_used2 = False
+    while answer == "y":
+        print("Enter the range of the number you want to guess. ")
+        min_range = ''
+        max_range = ''
+        while min_range == '':
+            min_range = input("Enter the minimum range from number:").strip()
+            if min_range.isdigit():
+                min_range = int(min_range)
+            else:
+                print('You did not enter a number, please enter a number next time!')
+                min_range = ''
+        while max_range == '':
+            max_range = input("Enter the maximum range from number:").strip()
+            if max_range.isdigit():
+                max_range = int(max_range)
+            else:
+                print('You did not enter a number, please enter a number next time!')
+                max_range = ''
+        # check that min is less than max
+        if min_range > max_range:
+            print('You really need to know what minimum and maximum means, '
+                  'just to help you I am going to swap the numbers around '
+                  'in order to have a game today and not next week!')
+            temp_holder = max_range
+            max_range = min_range
+            min_range = temp_holder
+        if min_range == max_range:
+            print('There is really is no hope for you, the number had to be different')
+            print('So I am going to use my initiative and make your minimum 1 and your maximum 1000')
+            min_range = 1
+            max_range = 1000
+        print(f'Hey now your minimum is {min_range} and your maximum is {max_range}')
+        play_again = game(min_range, max_range)
+        answer = input("Do you want to play again? (y/n)")
+        if answer == 'n':
+            print("Thank you for playing. Goodbye!")
+            break
