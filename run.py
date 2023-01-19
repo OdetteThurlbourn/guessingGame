7f3f765e2090a2e7725016a0699aebb81ce547bd
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-19 18:24:45 +0000

    Add  statement

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
 import random
 
+
 # Making sure guess value is an integer
 def validate_guess(guess):
-    if not guess.isdigit():
-        return False
-    else:
+    if guess.isdigit():
+        if not min_range <= int(guess) <= max_range:
+            print(f"You need to enter a number... you managed that part, but the number needs "
+                  f"to be in the range from {min_range} to {max_range}")
+            return False
         return True
+    return False
 
-def game(min_range, max_range):
-    correct_guesses = 0
-    total_tries = 0
-    hint_used = False
-    hint_used2 = False
-    number = random.randint(min_range, max_range)
+
+def game(min_inputted, max_inputted):
+    number = random.randint(min_inputted, max_inputted)
     guesses = 0
     while True:
         # Get users guess
@@ -22,90 +23,77 @@ def game(min_range, max_range):
             continue
         guess = int(guess)
         guesses += 1
-        total_tries += guesses
-
+        another_go = "Guess again"
+        if guesses == 9:
+            another_go = "Oh dear..."
         if guess == number:
-            correct_guesses += 1
-            print("Congratulations! You guessed the correct number in", guesses, "tries.")
-            hint_used = False
-            hint_used2 = False
-            play_again = input
-            guesses = 0
+            plural = "tries"
+            if guesses == 1:
+                plural = "try"
+            print(f"Congratulations! You guessed the correct number in {guesses} {plural}.")
             break
         elif guess > number:
-            print("Too high! Guess again.")
+            print(f"Too high! {another_go}.")
         else:
-            print("Too low! Guess again.")
+            print(f"Too low! {another_go}.")
 
-        if guesses == 3 and hint_used == False:
+        if guesses == 3:
             hint = input("Would you like a hint? (y/n)")
             if hint.lower() == "y":
-                hint_used = True
                 if number % 2 == 0:
                     print("Hint: the answer is even.")
                 else:
                     print("Hint: the answer is odd.")
-        if guesses == 6 and hint_used2 == False:
-            hint = input("Would you like another hint? (y/n)")
+        if guesses == 6:
+            hint = input("Would you like hint? (y/n)")
             if hint.lower() == "y":
-                hint_used2 = True
                 print("Hint: the answer is a number! \U0001f60a")
 
-        if guesses == 9 and hint_used == hint_used2 and guess != number:
-            print("Sorry! Game Over!")
+        if guesses == 9:
+            print("Wow! I can't believe in nine guesses you couldn't get it...lol...Game Over!")
             break
-        print(f"Guesses: {guesses}")
-        print(f"Hint Used: {hint_used}")
-        print(f"Guess: {guess}")
-        print(f"Number: {number}")
-        print(f"hint_used2: {hint_used2}")
-            
+        print(f"You've had {guesses} Guesses so far, only {9 - guesses} to go.")
 
-    return correct_guesses, total_tries, hint_used
 
-# The code below was in collaboration with Mentor Spencer Barriball
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Welcome to my guessing game! Try to guess the number and win....nothing!")
     answer = "y"
     correct_guesses = 0
     total_tries = 0
-    hint_used = False
-    hint_used2 = False
     while answer == "y":
         print("Enter the range of the number you want to guess. ")
-        min_range = ''
-        max_range = ''
-        while min_range == '':
+        min_range = ""
+        max_range = ""
+        while min_range == "":
             min_range = input("Enter the minimum range from number:").strip()
             if min_range.isdigit():
                 min_range = int(min_range)
             else:
-                print('You did not enter a number, please enter a number next time!')
-                min_range = ''
-        while max_range == '':
+                print("You did not enter a number, please enter a number next time!")
+                min_range = ""
+        while max_range == "":
             max_range = input("Enter the maximum range from number:").strip()
             if max_range.isdigit():
                 max_range = int(max_range)
             else:
-                print('You did not enter a number, please enter a number next time!')
-                max_range = ''
+                print("You did not enter a number, please enter a number next time!")
+                max_range = ""
         # check that min is less than max
         if min_range > max_range:
-            print('You really need to know what minimum and maximum means, '
-                  'just to help you I am going to swap the numbers around '
-                  'in order to have a game today and not next week!')
+            print("You really need to know what minimum and maximum means, "
+                  "just to help you I am going to swap the numbers around "
+                  "in order to have a game today and not next week!")
             temp_holder = max_range
             max_range = min_range
             min_range = temp_holder
         if min_range == max_range:
-            print('There is really is no hope for you, the number had to be different')
-            print('So I am going to use my initiative and make your minimum 1 and your maximum 1000')
+            print("There is really is no hope for you, the number had to be different")
+            print("So I am going to use my initiative and make your minimum 1 and your maximum 1000")
             min_range = 1
             max_range = 1000
-        print(f'Hey now your minimum is {min_range} and your maximum is {max_range}')
-        play_again = game(min_range, max_range)
+        print(f"Hey now your minimum is {min_range} and your maximum is {max_range}")
+        game(min_range, max_range)
         answer = input("Do you want to play again? (y/n)")
-        if answer == 'n':
+        if answer == "n":
             print("Thank you for playing. Goodbye!")
-            break
-
+            exit()
