726d18d0b1774d258d6c91a80d3e698bec5a8b7d
OdetteThurlbourn <odette.thurlbourn@gmail.com>
2023-01-20 09:53:28 +0000

    Fix whitespace and indentation errors

--- run.py
+++ run.py
@@ -6,12 +6,13 @@ def validate_guess(guess):
     if guess.isdigit():
         if not min_range <= int(guess) <= max_range:
             print(f"You need to enter a number... you managed that part, but the number needs "
-                  f"to be in the range from {min_range} to {max_range}")
+                    f"to be in the range from {min_range} to {max_range}")
             return False
         return True
     return False
 
 
+# Defining the random number, min and max range
 def game(min_inputted, max_inputted):
     number = random.randint(min_inputted, max_inputted)
     guesses = 0
@@ -21,9 +22,11 @@ def game(min_inputted, max_inputted):
         if not validate_guess(guess):
             print("Invalid input, please enter a number.")
             continue
+        # Tally users guess
         guess = int(guess)
         guesses += 1
         another_go = "Guess again"
+        # Keeps track of guesses so it knows when to prompt for a hint
         if guesses == 9:
             another_go = "Oh dear..."
         if guess == number:
@@ -32,20 +35,21 @@ def game(min_inputted, max_inputted):
                 plural = "try"
             print(f"Congratulations! You guessed the correct number in {guesses} {plural}.")
             break
+        # Clues for user before hints are prompted
         elif guess > number:
             print(f"Too high! {another_go}.")
         else:
             print(f"Too low! {another_go}.")
-
+        # Breaking loop after failed attempts with 2 hints
         if guesses == 3:
-            hint = input("Would you like a hint? (y/n)")
+            hint = input("Would you like a hint? (y / n)")
             if hint.lower() == "y":
                 if number % 2 == 0:
                     print("Hint: the answer is even.")
                 else:
                     print("Hint: the answer is odd.")
         if guesses == 6:
-            hint = input("Would you like hint? (y/n)")
+            hint = input("Would you like hint? (y / n)")
             if hint.lower() == "y":
                 print("Hint: the answer is a number! \U0001f60a")
 
@@ -54,6 +58,9 @@ def game(min_inputted, max_inputted):
             break
         print(f"You've had {guesses} Guesses so far, only {9 - guesses} to go.")
 
+
+""" This code block is part of the main function of the program and serves as the initial setup 
+for the guessing game."""
 
 if __name__ == "__main__":
     print("Welcome to my guessing game! Try to guess the number and win....nothing!")
@@ -78,22 +85,27 @@ if __name__ == "__main__":
             else:
                 print("You did not enter a number, please enter a number next time!")
                 max_range = ""
-        # check that min is less than max
+
+
+        """ This code block checks that the minimum value entered is less than the maximum value entered.
+        If the user failed to differentiate between the two, the game will swap the two value around
+        and the game ill commence. """
+
         if min_range > max_range:
             print("You really need to know what minimum and maximum means, "
-                  "just to help you I am going to swap the numbers around "
-                  "in order to have a game today and not next week!")
+                    "just to help you I am going to swap the numbers around "
+                    "in order to have a game today and not next week!")
             temp_holder = max_range
             max_range = min_range
             min_range = temp_holder
         if min_range == max_range:
-            print("There is really is no hope for you, the number had to be different")
+            print("There really is no hope for you, the number had to be different")
             print("So I am going to use my initiative and make your minimum 1 and your maximum 1000")
             min_range = 1
             max_range = 1000
         print(f"Hey now your minimum is {min_range} and your maximum is {max_range}")
         game(min_range, max_range)
-        answer = input("Do you want to play again? (y/n)")
+        answer = input("Do you want to play again? (y / n)")
         if answer == "n":
             print("Thank you for playing. Goodbye!")
             exit()
